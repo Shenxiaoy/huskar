05b99ab23d07feb8df9f10a017f782f1ddbf1444
Shenxiaoy <altersxy@126.com>
2020-06-19 15:06:46 +0800

    ss

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,40 +1,3 @@
-##
-
-### 配置示例一
-```js
-module.exports = {
-  title: '鹤山市',
-  description: '山东省立方',
-  themeConfig: {
-    nav: [
-      {text: 'guide', link: '/guide/'}
-    ],
-    sidebar: [
-      {
-        title: '测试guide',
-        collapsable: false, // 不折叠
-        sidebarDepth: 1,  // h标题深度展示，1展示h2,2展示到h3
-        // displayAllHeaders: true, // 全部展开
-        children: [
-          {
-            title: '介绍',
-            path: '/guide/'
-          },
-          {
-            title: '测试1',
-            path: '/guide/test1/'
-          }
-        ]
-
-      }
-    ]
-  }
-}
-```
-
-### 多个侧边栏
-
-```js
 module.exports = {
   title: '鹤山市',
   description: '山东省立方',
@@ -74,5 +37,3 @@ module.exports = {
     }
   }
 }
-
-```
