36e2df8e8bff727065e678371926e99986b48d3e
unknown <shenxiaoyu3>
2021-09-18 17:17:13 +0800

    test

--- html/js/test.js
+++ html/js/test.js
@@ -1,26 +1,8 @@
-
-var firstMissingPositive = function (nums) {
-  var len = nums.length;
-  if (len <= 0) {
-    return 1;
-  }
-  for (var i = 0; i < len; i++) {
-    if (nums[i] > 0) {
-      while(nums[i] < i + 1 && nums[i] != nums[nums[i] -1]) {
-        var tempIndex = nums[i] - 1;
-        var temp = nums[i];
-        nums[i] = nums[tempIndex];
-        nums[tempIndex] = temp;
-      }
-    }
-  }
-
-  for (var i = 0; i < len; i++) {
-    if (nums[i] !== i + 1) {
-      return i + 1;
-    }
-  }
-
-  return nums.length + 1;
-};
-console.log('=====+++', firstMissingPositive([5,6,7]))
+function a (l,m) {
+  console.log(l,m)
+}
+const obj = {
+  name: 'obj'
+}
+const b = a.bind(obj, 'sxy', 'ssss')
+b(5,7)
